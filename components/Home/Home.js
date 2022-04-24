3d0799baa9ea4584668abf9fd76e6bddf41d57ae
Camilo292 <caminore22@gmail.com>
2022-04-24 16:48:00 -0500

    Home component

---
+++ components/Home/Home.js
@@ -0,0 +1,82 @@
+import styles from './Home.module.css'
+import Image from 'next/image'
+import Video from 'twilio-video'
+import { useState, useContext } from 'react'
+import { useRouter } from 'next/router'
+import { RoomContext } from '../../Context/RoomContext'
+
+function Home () {
+  const [loading, setLoading] = useState(false)
+  const { setRoom } = useContext(RoomContext)
+  const router = useRouter()
+
+  const handleSubmit = async (evt) => {
+    setLoading(true)
+    evt.preventDefault()
+    const roomName = evt.target.room.value
+    const data = await fetch('/api/get-token', {
+      method: 'POST',
+      body: JSON.stringify({
+        identity: 'username',
+        room: roomName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const response = await data.json()
+    const token = response.token
+    Video.connect(token, {
+      name: roomName
+    }).then((room) => {
+      setRoom(room)
+      router.push(`/${roomName}`)
+    }).catch(err => {
+      console.log(err)
+      setLoading(false)
+    })
+  }
+  const date = new Intl.DateTimeFormat('en-US').format(Date.now())
+  return (
+    <>
+      <header className={styles.header}>
+        <ul className={styles.list}>
+          <li className={styles.logo}>
+            <Image src='/meetLogo.png' alt='meet Logo' height={33} width={58}/>
+            <p>
+              Twilio
+              <span> Meet</span>
+            </p>
+          </li>
+          <li className={styles.date}>{date}</li>
+          <li>
+            <Image src='/user.png' alt='user iamge' height={33} width={33} className={styles.image}/>
+          </li>
+        </ul>
+      </header>
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <h3>
+            Video conferencias premium. Ahora gratis para todos
+          </h3>
+          <p>
+            Rediseñamos Google Meet, nuestro servicio de reuniones de negocios seguras, de modo que sea gratuito y esté disponible para todos.
+          </p>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <input type="text" id='room' name='room' required placeholder='Nombre de la sala' />
+            {!loading && <button>Ingresar!</button> }
+            {loading && <button disabled >Connectando...</button> }
+          </form>
+        </section>
+        <aside className={styles.aside}>
+          <Image src='/mainAside.svg' alt='Aside logo' width={330} height={330} />
+          <h4>Obtén un vinculo para compartir</h4>
+          <p>Haz click en nueva reunion para obtener un vínculo que puedas enviar a las personas con quienes quieras reunirte</p>
+        </aside>
+      </main>
+
+    </>
+  )
+}
+
+export { Home }
