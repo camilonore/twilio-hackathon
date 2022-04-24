37e688225799fc011e0b4752e13a04cd95d62d3f
Camilo292 <caminore22@gmail.com>
2022-04-24 16:46:51 -0500

    get-token endpoint

---
+++ pages/api/get-token.js
@@ -0,0 +1,29 @@
+import { videoToken } from '../../utils/videoToken'
+
+export default function handler (req, res) {
+  const sendTokenResponse = (token, res) => {
+    return res.send(
+      JSON.stringify({
+        token: token.toJwt()
+      })
+    )
+  }
+
+  if (req.method === 'GET') {
+    const identity = req.query.identity
+    const room = req.query.room
+    if (!identity || !room) {
+      return res.status(400).send('Identity and Room required')
+    }
+    const token = videoToken(identity, room)
+    sendTokenResponse(token, res)
+  } else if (req.method === 'POST') {
+    const identity = req.body.identity
+    const room = req.body.room
+    if (!identity || !room) {
+      return res.status(400).send('Identity and Room required')
+    }
+    const token = videoToken(identity, room)
+    sendTokenResponse(token, res)
+  }
+}
