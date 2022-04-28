4da7ecece03ac032c4b0abd092a8968f9e60c9d1
Camilo292 <caminore22@gmail.com>
2022-04-28 15:28:02 -0500

    Fixed bugs

--- components/Room/VideoCall/VideoCall.js
+++ components/Room/VideoCall/VideoCall.js
@@ -4,18 +4,15 @@ import { User } from '../../User/User'
 import { useRouter } from 'next/router'
 import styles from './VideoCall.module.css'
 import { Footer } from '../Footer/Footer'
-import { useVideo } from '../../../hooks/useVideo'
 
 function VideoCall () {
   const { room, users } = useContext(RoomContext)
   const router = useRouter()
   const { roomName } = router.query
-  const { loading } = useVideo({ roomName })
 
   return (
     <div className={styles.room}>
       <main className={users.length > 0 ? styles.main : styles.alone}>
-        {loading && <p style={{ color: 'white' }}>Loading...</p>}
         {room && <User userStyles={'user'} participant={room.localParticipant}/>}
         {users.length > 0 && users.map(user => {
           return <User userStyles={'user'} participant={user} key={user.sid}/>
